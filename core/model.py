681c11b956be955e098ea03db7f487923b818ed2
QuyAnh <dangquyanh150101@gmail.com>
2024-06-12 01:35:36 +0700

    add models for node classification

--- core/model.py
+++ core/model.py
@@ -3,6 +3,19 @@ import torch.nn.functional as F
 from torch_geometric.nn import SAGEConv, GCNConv, GATConv, SuperGATConv
 
 
+# SAGE for node classification
+class GraphSAGE(torch.nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(GraphSAGE, self).__init__()
+        self.conv1 = SAGEConv(in_channels, 2 * out_channels)
+        self.conv2 = SAGEConv(2 * out_channels, out_channels)
+
+    def forward(self, x, edge_index):
+        x = F.relu(self.conv1(x, edge_index))
+        x = F.dropout(x, training=self.training)
+        x = self.conv2(x, edge_index)
+        return F.log_softmax(x, dim=1)
+
 # SAGE for link prediction
 class GraphSAGEEncoder(torch.nn.Module):
     def __init__(self, in_channels, out_channels):
@@ -14,6 +27,20 @@ class GraphSAGEEncoder(torch.nn.Module):
     def forward(self, x, edge_index):
         x = F.relu(self.conv1(x, edge_index))
         return self.conv_mu(x, edge_index), self.conv_logstd(x, edge_index)
+    
+
+# GCN for node classification
+class GCN(torch.nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(GCN, self).__init__()
+        self.conv1 = GCNConv(in_channels, 2 * out_channels)
+        self.conv2 = GCNConv(2 * out_channels, out_channels)
+
+    def forward(self, x, edge_index):
+        x = F.relu(self.conv1(x, edge_index))
+        x = F.dropout(x, training=self.training)
+        x = self.conv2(x, edge_index)
+        return F.log_softmax(x, dim=1)
 
 # GCN for link prediction
 class GCNEncoder(torch.nn.Module):
@@ -27,6 +54,20 @@ class GCNEncoder(torch.nn.Module):
         x = F.relu(self.conv1(x, edge_index))
         return self.conv_mu(x, edge_index), self.conv_logstd(x, edge_index)
 
+
+# GAT for node classification
+class GAT(torch.nn.Module):
+    def __init__(self, in_channels, out_channels, heads=1):
+        super(GAT, self).__init__()
+        self.conv1 = GATConv(in_channels, 2 * out_channels, heads=heads)
+        self.conv2 = GATConv(2 * out_channels * heads, out_channels, heads=1)
+
+    def forward(self, x, edge_index):
+        x = F.elu(self.conv1(x, edge_index))
+        x = F.dropout(x, training=self.training)
+        x = self.conv2(x, edge_index)
+        return F.log_softmax(x, dim=1)
+
 # GAT for link prediction
 class GATEncoder(torch.nn.Module):
     def __init__(self, in_channels, out_channels, heads=1):
@@ -38,6 +79,20 @@ class GATEncoder(torch.nn.Module):
     def forward(self, x, edge_index):
         x = F.elu(self.conv1(x, edge_index))
         return self.conv_mu(x, edge_index), self.conv_logstd(x, edge_index)
+    
+
+# SuperGAT for node classification
+class SuperGAT(torch.nn.Module):
+    def __init__(self, in_channels, out_channels, heads=1):
+        super(SuperGAT, self).__init__()
+        self.conv1 = SuperGATConv(in_channels, 2 * out_channels, heads=heads)
+        self.conv2 = SuperGATConv(2 * out_channels * heads, out_channels, heads=1)
+
+    def forward(self, x, edge_index):
+        x = F.elu(self.conv1(x, edge_index))
+        x = F.dropout(x, training=self.training)
+        x = self.conv2(x, edge_index)
+        return F.log_softmax(x, dim=1)
 
 # SuperGAT for link prediction
 class SuperGATEncoder(torch.nn.Module):
